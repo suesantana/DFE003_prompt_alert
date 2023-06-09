c5031dfae9c4cdba3067359b3ed72e765548dd78
Larissa Maranho <larissamaranhor@gmail.com>
2023-06-09 19:34:38 -0300

    problem solved

--- src/main.js
+++ src/main.js
@@ -10,37 +10,36 @@ if (numero % 2 === 0){
 if (numero % 2 !== 0){
     alert (fraseImpar);
 }
-const frase = `O dobro de ${numero} é ${dobro}`;
-alert (frase);
 
-//- Elabore um programa que calcule o valor do jantar, sendo o valor total do jantar composto pela conta junto com a taxa de atendimento do garçom. 
+// //- Elabore um programa que calcule o valor do jantar, sendo o valor total do jantar composto pela conta junto com a taxa de atendimento do garçom.
+//   Entrada de dados: ler o valor da conta.
+//   Processamento: calcular junto com o valor do custo do garçom.
+//   Saída: informar o resultado.
+// Adicionar uma pergunta para saber qual dia da semana que é hoje. Se for sábado ou domingo a taxa do garçom será 20%, senão (ou seja, os outros dias da semana) será 10%.
+// Sendo sábado ou domingo, retorne uma frase para aparecer na tela:
+// “Hoje é sábado. A taxa do garçom é 20%. O valor total ficou em ….”.
+// Se for outro dia da semana:
+// “Hoje é terça. A taxa do garçom é 10%. O valor total ficou em ….”.
 const valorDaConta = Number(window.prompt("Qual o valor da conta?"));
-//const diaDaSemana = prompt("Que dia da semana é hoje?");
+const diaDaSemana = prompt("Que dia da semana é hoje?");
 const valorDiasUteis = valorDaConta * 0.1;
-//const valorFimDeSemana = valorDaConta * 0.2;
+const valorFimDeSemana = valorDaConta * 0.2;
 const totalDiasUteis = valorDaConta + valorDiasUteis;
-const fraseTotal = `O valor a ser pago é de R$ ${totalDiasUteis}.`;
-alert (fraseTotal);
-//const totalFds = valorDaConta + valorFimDeSemana;
-//const diasFds = ["sábado","domingo"];
-//const diasUteis = ["segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira","SEGUNDA","TERÇA","QUARTA","QUINTA","SEXTA"];
-//const fraseDiasUteis = `Hoje é ${diaDaSemana}. A taxa do garçom é 10%. O valor total ficou em ${totalSemana}`;
-//const fraseFds = `Hoje é ${diaDaSemana}. A taxa do garçom é 20%. O valor total ficou em ${totalFds}`;
-//encontrar um while pra enquanto não digitar um dia da semana válido mostrar um alert de erro e retornar para a caixinha de pergunta
-// const fraseErro = `Digite um dia da semana válido.`
-// while (diaDaSemana !== diasUteis && 
-//     diaDaSemana !== diasFds){
-//     alert ("Digite um dia válido");
-//     diaDaSemana = prompt("Que dia da semana é hoje?");
-// }
-// if (diaDaSemana === "Sábado" ||
-// diaDaSemana === "Domingo") {
-//     alert (fraseFds)}; 
-// if (diaDaSemana == diasUteis) {
-//     alert (fraseDiasUteis);
-// }
+const totalFds = valorDaConta + valorFimDeSemana;
+const diasFds = ["sábado", "domingo"];
+const diasUteis = ["segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "SEGUNDA", "TERÇA", "QUARTA", "QUINTA", "SEXTA"];
+const fraseDiasUteis = `Hoje é ${diaDaSemana}. A taxa do garçom é 10%. O valor total ficou em ${totalDiasUteis}.`;
+const fraseFds = `Hoje é ${diaDaSemana}. A taxa do garçom é 20%. O valor total ficou em ${totalFds}.`;
+const fraseErro = `Digite um dia da semana válido.`;
 
-//else (alert (fraseErro));
+if (diasFds.includes(diaDaSemana.toLowerCase())) {
+    alert(fraseFds);
+} else if (diasUteis.includes(diaDaSemana.toUpperCase())) {
+    alert(fraseDiasUteis);
+} else {
+    alert(fraseErro);
+}
+
 
 //const valorComTaxa = valorDaConta * 0.1;
 //const valorTotal = valorDaConta + valorComTaxa;
